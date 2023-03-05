60487be3204e6abcc505511f6d39f9ea81eb1610
Dirk Kok <the@dirkkok.nl>
2023-03-05 22:03:31 +0100

    fixed test name

--- CorporateEspionage.Tests/ReflectionUtilTests.cs
+++ CorporateEspionage.Tests/ReflectionUtilTests.cs
@@ -4,7 +4,7 @@ namespace CorporateEspionage.Tests;
 
 public class ReflectionUtilTests {
 	[Test]
-	public void CanGenerateSpy() {
+	public void CanFindInterfaceDeclarationsForMethod() {
 		MethodInfo interfaceTest1 = typeof(ITestInterface).GetMethod("Test1")!;
 		MethodInfo classTest1     = typeof(TestClass).GetMethod("Test1")!;
 		MethodInfo? classTest2    = classTest1.GetInterfaceDeclarationsForMethod().FirstOrDefault();
