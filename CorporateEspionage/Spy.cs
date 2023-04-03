434bc691b359ec314ccd758aaf7dfac27104a8e2
Dirk Kok <the@dirkkok.nl>
2023-04-03 19:48:47 +0200

    and more

--- CorporateEspionage/Spy.cs
+++ CorporateEspionage/Spy.cs
@@ -13,12 +13,20 @@ public class Spy<T> where T : class {
 		m_SpiedObject = @object as SpiedObject ?? throw new InvalidCastException($"Cannot cast T ({typeof(T).FullName}) to SpiedObject");
 	}
 
+	public int GetCallCount(MethodInfo method) {
+		return m_SpiedObject.Calls.TryGetValue(method, out List<CallParameters>? calls) ? calls.Count : 0;
+	}
+
+	public int GetCallCount() {
+		return m_SpiedObject.Calls.Sum(kvp => kvp.Value.Count);
+	}
+
 	public IReadOnlyDictionary<MethodInfo, IReadOnlyList<CallParameters>> GetCalls() {
 		return m_SpiedObject.Calls.WrapReadonly();
 	}
 
 	public IReadOnlyList<CallParameters> GetCalls(MethodInfo method) {
-		return m_SpiedObject.Calls[method];
+		return m_SpiedObject.Calls[method].AsReadonly();
 	}
 	
 	public CallParameters GetCallParameters(MethodInfo method, int invocation) {
